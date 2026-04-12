7ab12684f8730a83540d5d073f59b4dec76ced70
Kimpoy1525 <krystiandeleon15@gmail.com>
2026-04-12 13:03:40 +0800

    Fixed documents urls

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import DocumentDeleteView, DocumentUploadView, DocumentListView, DocumentDetailView, DocumentUpdateView
+from .views import (
+    DocumentDeleteView, 
+    DocumentUploadView, 
+    DocumentListView, 
+    DocumentDetailView, 
+    DocumentUpdateView,
+    FileDownloadView  
+)
 
 urlpatterns = [
     path('upload/', DocumentUploadView.as_view(), name='document-upload'),
@@ -7,4 +14,5 @@ urlpatterns = [
     path('detail/<int:id>/', DocumentDetailView.as_view(), name='document-detail'),
     path('detail/<int:id>/update/', DocumentUpdateView.as_view(), name='document-update'),
     path('detail/<int:id>/delete/', DocumentDeleteView.as_view(), name='document-delete'),
+    path('download/<int:file_id>/', FileDownloadView.as_view(), name='file-download'),
 ]
