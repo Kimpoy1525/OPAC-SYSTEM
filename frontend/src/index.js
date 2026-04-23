1f680409aa5750f5c1df922097d18a5a3a243adb
Kimpoy1525 <krystiandeleon15@gmail.com>
2026-04-23 21:40:10 +0800

    v1.0 with editlog, deletelog, and uploadlog finalize

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';   // ⬅️ add this
+import axios from 'axios';
+
+// Enable sending cookies/session with every axios request
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
